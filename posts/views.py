5452f5f0ee44f68bed82be024e42165f61c0b2cf
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-23 22:00:02 +0530

    Created post detail view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Post
+from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
-# Create your views here.
+# Listing all the blogs
+class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+# Post Detail Endpoint
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    lookup_field = 'slug'
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
