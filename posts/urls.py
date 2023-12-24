642c8d051927d16479882eb608667eecd8ff6f41
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-24 22:43:49 +0530

    Added enpoints using routers

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import PostList, PostDetail
+# from .views import PostList, PostDetail, UserList, UserDetail
+from .views import PostViewSet, UserViewSet
 
-urlpatterns = [
-    path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
-    path('', PostList.as_view(), name='post_list'),
-]
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('', PostList.as_view(), name='post_list'),
+#     path('users/', UserList.as_view(), name='user_list'),
+#     path('users/<slug:username>/', UserDetail.as_view(), name='user_detail'),
+#     path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+# ]
