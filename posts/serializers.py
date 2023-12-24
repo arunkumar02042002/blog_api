ceba1c09140e2b4b60f029a68f343464d5d4cebb
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-24 22:44:07 +0530

    Created UserSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class PostSerializer(serializers.ModelSerializer):
 
@@ -7,3 +10,9 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ('id', 'author', 'title', 'body', 'thumbnail', 'slug', 'created_at')
         read_only_fields = ('author', 'slug',)
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username')
