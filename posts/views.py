23866bbd1abe5aa4a40a7818888bcc0215a418a1
Arun Kumar <arun.kumar.2403gg@gmail.com>
2023-12-24 22:43:27 +0530

    Added enpoints for users using viewsets

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,49 @@
-from rest_framework import permissions
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# Third Party imports
+# from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# from rest_framework import permissions
+from rest_framework import viewsets
+
+# Project level imports
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
-# Listing all the blogs
-class PostList(ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# Django Imports
+from django.contrib.auth import get_user_model
 
-# Post Detail Endpoint
-class PostDetail(RetrieveUpdateDestroyAPIView):
+User = get_user_model()
+
+# # Listing all the blogs
+# class PostList(ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# # Post Detail Endpoint
+# class PostDetail(RetrieveUpdateDestroyAPIView):
+#     permission_classes = (IsAuthorOrReadOnly,)
+#     lookup_field = 'slug'
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+'''
+Utilizing the Viewsets to wrap the above views into a single viewset
+'''
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     lookup_field = 'slug'
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+# class UserList(ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+# class UserDetail(RetrieveUpdateDestroyAPIView):
+#     lookup_field = 'username'
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+    
+class UserViewSet(viewsets.ModelViewSet):
+    lookup_field = 'username'
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
